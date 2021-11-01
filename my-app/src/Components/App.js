c28b5b5a73373952aa4a9f5dc33a90261ea995f9
YahavSofer <yahavss93@gmail.com>
2021-11-01 15:37:14 +0200

    starting work

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -13,8 +13,8 @@ import UpdateProfile from './UpdateProfile'
 function App() {
   return (
     
-        <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}}>
-          <div className='w-100' style={{maxWidth:"360px"}}>
+        <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}} >   {/**/}
+          <div className='w-100'>
           
                   <Router>
                       <AuthProvider>
--- my-app/src/Components/Login.js
+++ my-app/src/Components/Login.js
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from 'react'
-import {Alert, Button, Card, Form} from 'react-bootstrap'
+import {Alert, Button, Card,Form,Container,Row,Col} from 'react-bootstrap'
 import { useAuth } from '../context/AuthContext'
 import { Link , useHistory } from 'react-router-dom'
 
@@ -27,33 +27,43 @@ export default function Login() {
     }
     return (
         <>
-        <Card>
-            <Card.Body>
-                <h2 className="text-center mb-4">Log In</h2>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit = {handleSubmit}>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} required/> 
-                    </Form.Group>    
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' ref={passwordRef} required/> 
-                    </Form.Group>
-                    
-                    <Button disabled={loading} type='submit' className='w-100'>
-                        Log In
-                    </Button>
-                    <div className="w-100 text-center mt-2">
-                        <Link to='/forgot-password'>Forgot password ?</Link>
+    <Container> 
+        <Row>
+            <Col style={{minWidth:'350px',maxWidth:'400px'}}>
+                    <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Log In</h2>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit = {handleSubmit}>
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required/> 
+                            </Form.Group>    
+                            <Form.Group id="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type='password' ref={passwordRef} required/> 
+                            </Form.Group>
+                            
+                            <Button disabled={loading} type='submit' className='w-100'>
+                                Log In
+                            </Button>
+                            <div className="w-100 text-center mt-2">
+                                <Link to='/forgot-password'>Forgot password ?</Link>
+                            </div>
+                        </Form>               
+                    </Card.Body>
+                </Card>
+                    <div className="w-100 text-center mt-2"> 
+                        Don't have account ? <Link to='/signup'>Sign Up</Link>    
                     </div>
-                </Form>               
-            </Card.Body>
-        </Card>
-            <div className="w-100 text-center mt-2"> 
-                Don't have account ? <Link to='/signup'>Sign Up</Link>    
-            </div>
-        {/* hello world */}
+        </Col>
+        <Col className='m-sm-4 bg-info text-sm-center'>
+            sdfkhlksdj
+            sadslkk
+        </Col>
+        </Row>
+    </Container>
+
     </>
     )
 }
