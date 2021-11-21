0328f70d5cc263311e9f0957cdf285ab6791461c
YahavSofer <yahavss93@gmail.com>
2021-11-21 20:08:12 +0200

    start HomePage

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -11,37 +11,45 @@ import UpdateProfile from './UpdateProfile'
 import UserForm from './UserForm'
 import MessageSignUp from './MessageSignUp'
 import CreateEvent from './CreateEvent'
+import HomePage from "./HomePage/HomePage";
+
 
 
 function App() {
   return (
-        
-        <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}} >   {/**/}
-          <div className='w-100'>
-              
+
+          
                   <Router>
                       <AuthProvider>
                       
                         <Switch>
-                            <PrivateRoute exact path="/" component={Dashboard} />
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            
+
                             <PrivateRoute  path="/update-profile" component={UpdateProfile} />
                             <PrivateRoute  path="/create-event" component={CreateEvent} />
                             
-                            <Route path="/signup" component={Signup} />
-                            <Route path="/messagesignup" component={MessageSignUp} />
-                            <PrivateRoute path="/userform" component={UserForm} />
-                            
-                            <Route path="/login" component={Login} />
-                            <Route path="/forgot-password" component={ForgotPassword} />
 
+
+                            
+                                    
+                            <Container className = 'd-flex align-items-center justify-content-center' style={{minHeight:"100vh"}} >   {/**/}
+                            <div className='w-100'>
+                                <Route path="/signup" component={Signup} />
+                                <Route path="/messagesignup" component={MessageSignUp} />
+                                <PrivateRoute path="/userform" component={UserForm} />
+                                <Route path="/login" component={Login} />
+                                <Route path="/forgot-password" component={ForgotPassword} />
+                            </div>
+                            </Container>
                         </Switch>
                         
                       </AuthProvider>
                   </Router>
              
-          </div>
           
-        </Container>
+          
+        
     
   );
 }
---
+++ my-app/src/Components/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import React , {useRef, useState} from 'react'
+import { Container } from 'react-bootstrap'
+import NavBar from './NavBar'
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+
+
+export default function HomePage() {
+    return (
+    <div>
+      <NavBar/>
+        <Container>
+        
+        <div style={{height:'1000px'}}>
+        </div>
+
+
+
+
+        </Container>
+    </div>
+    )
+}
