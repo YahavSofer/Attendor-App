807e75735819bf3fc07c9e436fb7f23ae41e4d2f
Yonazamir <Yonazamir21@gmail.com>
2021-12-19 21:28:31 +0200

    UpdateProfile

--- my-app/src/Components/HomePage/UserProfile.js
+++ my-app/src/Components/HomePage/UserProfile.js
@@ -1,13 +1,23 @@
-import React , {useState} from 'react'
+import React , {useState, useEffect} from 'react'
 import {Card , Button , Alert} from 'react-bootstrap'
 import {  useAuth } from '../../context/AuthContext'
 import { Link,useHistory } from 'react-router-dom'
+import {db} from '../../firebaseConfig'
+import { doc,getDoc,getDocs,collection,where,query} from "firebase/firestore"
+import Avatar from '@mui/material/Avatar'
+import { Container,Row,Col} from 'react-bootstrap'
+
 
 export default function UserProfile() {
     const [error, setError] = useState('')
     const {currentUser , logout} = useAuth()
     const history = useHistory()
-
+    const [profileImage,setProfileImage] = useState()
+    const [isProfilePic,setIsProfilePic] = useState(false)
+    const [firstName,setFirstName] = useState()
+    const [lastName,setLastName] = useState()
+    const [userGender,setGender] = useState()
+    const [userBirthDay,setBirthday] = useState()
 
     async function handleLogout(){
         setError('')
@@ -21,20 +31,69 @@ export default function UserProfile() {
 
     } 
 
+    useEffect(() => {
+        // onload - get all events from firestore
+      
+        // set user profile in event profile picture. if they dont have, place icon insted
+        const getUserProfileImg = async () => {
+          const userDoc = await getDoc(doc(db,'users',currentUser.uid))
+          .then( u =>{
+                      setProfileImage(u.data().profileImage);
+                      setFirstName(u.data().first);
+                      setLastName(u.data().last);
+                      setGender(u.data().gender)
+                      const d = (u.data().birthday.toDate())
+                      setBirthday(d.getDate()+ '-'+(d.getMonth()+1)+'-'+d.getFullYear())
+
+                      console.log(d.getDate());
+                     console.log(u.data());
+
+          } )
+            
+        };
+        getUserProfileImg();
+        
+        if (profileImage !== ""){
+          setIsProfilePic(true)
+        } 
+      }, []);
+
+    
     return (
-        <>
-            <Card>
+        <>  <div style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)',
+            minWidth: '600px'
+          }} >
+            <Card  >
                 <Card.Body>
-                <h2 className="text-center mb-4">Profile</h2>
+                <Row>
+                <h2 className="text-center mb-4" style={{display:"inline"}}>My Profile </h2>
+                </Row>
+                <Row>
+                <Col>
                 {error && <Alert variant="danger">{error}</Alert>}
-                <strong>Email : </strong>{currentUser.email}
-                <Link to='/update-profile' className="btn btn-primary w-100 mt-3">Update profile</Link>
+                <strong>Email : </strong>{currentUser.email}<br></br>
+                <strong>First Name : </strong>{firstName}<br></br>
+                <strong>Last Name : </strong>{lastName}<br></br>
+                <strong>Gender : </strong>{userGender}<br></br>
+                <strong>Birthday : </strong>{userBirthDay}<br></br>
+                <Link to='/user/update-profile' className="btn btn-primary " style={{marginTop:"10px"}}>Edit Profile</Link>
+                </Col>
+                <Col>
+                {isProfilePic?
+                <Avatar src={profileImage} aria-label="user-name" sx={{ width: 200, height: 200 }}/>   
+                :
+                <Avatar src="" aria-label="user-name"/>    
+                }
+                </Col>
+                </Row>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2"> 
                <Button variant='link' onClick={handleLogout}>Log Out</Button>  
             </div>
-
+            </div>
         </>
 
     )
